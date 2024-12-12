8e19f2ede5aa4bd79b27db71abdfad2ddcc0a16e
Carrie <cko60@stuy.edu>
2024-12-12 12:39:24 -0500

    implemented a few methods for Mage

--- Mage.java
+++ Mage.java
@@ -1,3 +1,43 @@
-public class Mage{
+public class Mage extends Adventurer{
+	private int mana, maxMana;
 
+	public Mage(String name){
+		super(name);
+	}
+	public Mage(String name, int hp){
+		super(name, hp);
+	}
+	public Mage(String name, int mana){
+		super(name);
+		this.mana = mana;
+		this.maxMana = mana;
+	}
+	public Mage(String name, int hp, int mana){
+		super(name, hp);
+		this.mana = mana;
+		this.maxMana = mana;
+	}
+
+
+	public String getSpecialName(){
+		return "mana";
+	}
+	public int getSpecial(){
+		return mana;
+	}
+	public void setSpecial(int n){
+		if(mana + n > maxMana){
+			mana = maxMana;
+		}
+		else{
+			mana = n;
+		}
+	}
+	public int getSpecialMax(){
+		return maxMana;
+	}
+
+	public String attack(Adventurer other){
+		other.applyDamage(2);
+	}
 }
