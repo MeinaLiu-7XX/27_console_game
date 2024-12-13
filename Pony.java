d17cd28af64a99c2449ce664e87d475ec6becea5
Meina Liu <meinal4@nycstudents.net>
2024-12-13 12:21:19 -0500

    Update Pony.java

--- Pony.java
+++ Pony.java
@@ -1,1 +1,38 @@
+public class Pony extends Adventurer {
+  private int defense;
+  private int criticalHits;
+    
+  public Pony(String name, int HP, int defense, int criticalHits) {
+    super(name, HP, maxHP);
+    defense = 50;
+    criticalHits = defense/5;
+    magicWandMax = 20;
+    magicWand = magicWandMax/2;
+  }  
 
+  public Pony(String name, int HP, int defense, int criticalHits) {
+    this("Pony", 20, 50, 10);
+  }
+  
+  public String getSpecialName(){
+    return "magicWand";
+  }
+
+  public int getSpecial(){
+    return magicWand;
+  }
+  
+  public void setSpecial(int n){
+    magicWand = n;
+  }
+
+  public int getSpecialMax(){
+    return magicWandMax;
+  }
+
+  public String(Adventurer other){
+    int damage = (int)(Math.random() * 10);
+    other.applyDamage(damage);
+    return this;
+  }
+}
