dfe5106ff7e26c083686578c910054ec2bc5ac71
Meina Liu <meinal4@nycstudents.net>
2024-12-13 12:00:03 -0500

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -1,14 +1,15 @@
 public class Warrior extends Adventurer {
   private int defense;
   private int criticalHits;
-  
-  // public static void main(String[] args) {
-    
-  // }
     
   public Warrior(String name, int HP, int maxHP, int defense, int criticalHits) {
     super(name, HP, maxHP);
     this.defense = defense;
     this.criticalHits = criticalHits;
   }  
+
+  public Warrior(String name, int hp){
+    this("Zoey", 50);  
+  }
+  
 }
